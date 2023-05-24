2f765996a80698294bc582b4bb3d8a4b190337cc
Edras <edrasalvarezoliva@gmail.com>
2023-05-24 10:05:58 -0600

    04.05.2023 EA_BD

--- 001_BaseDatos_Tablas.sql
+++ 001_BaseDatos_Tablas.sql
@@ -25,27 +25,28 @@ fechaRegistro datetime default getdate()
 )
 go
  
- create table RolMenu(
- idRolMenu int primary key identity(1,1),
- idRol int references Rol(idRol),
- idMenu int references Menu(idMenu),
- esActivo bit,
- fechaRegistro datetime default getdate()
- )
- go
+create table RolMenu(
+idRolMenu int primary key identity(1,1),
+idRol int references Rol(idRol),
+idMenu int references Menu(idMenu),
+esActivo bit,
+fechaRegistro datetime default getdate()
+)
+go
 
 create table Usuario(
 idUsuario int primary key identity(1,1),
-Codigo varchar(50),
-NomcbreCompleto varchar(50),
+codigo varchar(50),
+documento varchar (25),
+nombreCompleto varchar(100),
 correo varchar(50),
 telefono varchar(50),
 idRol int references Rol(idRol),
 urlFoto varchar(500),
 nombreFoto varchar(100),
 clave varchar(100),
-Lugar varchar(100),
-LugarTrabajo varchar(100),
+lugarVive varchar(100),
+lugarTrabajo varchar(100),
 esActivo bit,
 fechaRegistro datetime default getdate()
 )
@@ -54,6 +55,7 @@ go
 create table Ida(
 idIda int primary key identity(1,1),
 descripcion varchar(50),
+Precio decimal (10,2),
 esActivo bit,
 fechaRegistro datetime default getdate()
 )
@@ -62,6 +64,19 @@ go
 create table Retorno(
 idRetorno int primary key identity(1,1),
 descripcion varchar(50),
+Precio decimal (10,2),
+esActivo bit,
+fechaRegistro datetime default getdate()
+)
+go
+
+Create table Clientes(
+idCliente int primary key identity(1,1),
+documentoPasaporte varchar (25),
+nombreCompleto varchar(100),
+correo varchar(50),
+telefono varchar(50),
+nacionalidad varchar(100),
 esActivo bit,
 fechaRegistro datetime default getdate()
 )
@@ -85,32 +100,30 @@ fechaRegistro datetime default getdate()
 )
 go
 
-create table Venta(
-idVenta int primary key identity(1,1),
+create table VentaBoleto(
+idVentaBoleto int primary key identity(1,1),
 numeroVenta varchar(6),
 idTipoDocumentoVenta int references TipoDocumentoVenta(idTipoDocumentoVenta),
 idUsuario int references Usuario(idUsuario),
 documentoCliente varchar(10),
 nombreCliente varchar(20),
-Observaciones varchar(5000),
 subTotal decimal(10,2),
-impuestoTotal decimal(10,2),
-Total decimal(10,2),
+total decimal(10,2),
+observaciones varchar(5000),
 fechaRegistro datetime default getdate()
 )
 go
 
 create table DetalleVenta(
 idDetalleVenta int primary key identity(1,1),
-idVenta int references Venta(idVenta),
-IdIda int references Ida(idIda),
-FechaIda datetime,
-IdRetorno int references Retorno(idRetorno),
-FechaRetorno datetime,
+idVentaBoleto int references VentaBoleto(idVentaBoleto),
 NumeroBoleto int,
+idIda int references Ida(idIda),
+fechaIda datetime,
+idRetorno int references Retorno(idRetorno),
+fechaRetorno datetime,
 precio decimal(10,2),
 total decimal(10,2)
-fechaRegistro datetime default getdate()
 )
 go
 
@@ -123,7 +136,6 @@ nombre varchar(50),
 correo varchar(50),
 direccion varchar(50),
 telefono varchar(50),
-porcentajeImpuesto decimal(10,2),
 simboloMoneda varchar(5)
 )
 go
@@ -145,6 +157,7 @@ idUsuario int references Usuario(idUsuario),
 documentoCliente varchar(10),
 nombreCliente varchar(20),
 Observaciones varchar(5000),
+fechaRegistro datetime default getdate()
 )
 go
 
@@ -154,7 +167,28 @@ idReserva int references Reserva(idReserva),
 IdIda int references Ida(idIda),
 FechaIda datetime,
 IdRetorno int references Retorno(idRetorno),
-FechaRetorno datetime,
-NumeroBoleto int,
+FechaRetorno datetime
+)
+go
+
+create table Pago(
+idPago int primary key identity(1,1),
+numeroPago varchar(6),
+idUsuario int references Usuario(idUsuario),
+documento varchar (20),
+nombreCompleto varchar (100),
+subTotal decimal(10,2),
+total decimal(10,2),
+fechaRegistro datetime default getdate()
+)
+go
+
+create table DetallePago(
+idDetallePago int primary key identity(1,1),
+idPago int references Pago(idPago),
+mesPago datetime,
+pago decimal(10,2),
+subTotal decimal(10,2),
+total decimal(10,2)
 )
 go
